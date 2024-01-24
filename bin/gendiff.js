f1e9d795efac86862a7591b7603587d32e67158b
Danil B. <boevdanil99@gmail.com>
2024-01-24 12:15:20 +0300

    Third Step

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,18 +1,17 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+import fileParse from '../src/parse.js';
 
 const program = new Command();
 
 program
     .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
-    .option('-f, --format <type>', 'output format', 'stylish')
+    .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>', 'output format')
     .arguments('<filepath1> <filepath2>')
     .action((filepath1, filepath2) => {
-        const options = program.opts();
-        console.log(gendiff(filepath1, filepath2, `${options.format}`));
-    });
-
-program.parse();
+        console.log(gendiff(filepath1, filepath2, program.opts().format));
+    })
+    .parse();
