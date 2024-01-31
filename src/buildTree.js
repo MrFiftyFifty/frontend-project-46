4d1b8925b5da15e8959796c8adae650bc60c0905
Danil B. <boevdanil99@gmail.com>
2024-01-31 22:58:05 +0300

    Fixed Code2

--- src/buildTree.js
+++ src/buildTree.js
@@ -2,32 +2,29 @@ import _ from 'lodash';
 
 const buildTree = (obj1, obj2) => {
   const sortedUnicKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
-  let resultObj = []; // Initialize as an empty array
-  sortedUnicKeys.forEach((key) => {
+  return sortedUnicKeys.reduce((acc, key) => {
     const value1 = obj1[key];
     const value2 = obj2[key];
 
     if (!Object.hasOwn(obj1, key)) {
-      resultObj = [...resultObj, { key, value: value2, type: 'added' }];
-    } else if (!Object.hasOwn(obj2, key)) {
-      resultObj = [...resultObj, { key, value: value1, type: 'deleted' }];
-    } else if (value1 === value2) {
-      resultObj = [...resultObj, { key, value: value1, type: 'unchanged' }];
-    } else if (_.isObject(value1) && _.isObject(value2)) {
-      resultObj = [...resultObj, { key, value: buildTree(value1, value2), type: 'hasChild' }];
-    } else {
-      resultObj = [
-        ...resultObj,
-        {
-          key,
-          oldValue: value1,
-          value: value2,
-          type: 'changed',
-        },
-      ];
+      return [...acc, { key, value: value2, type: 'added' }];
+    } if (!Object.hasOwn(obj2, key)) {
+      return [...acc, { key, value: value1, type: 'deleted' }];
+    } if (value1 === value2) {
+      return [...acc, { key, value: value1, type: 'unchanged' }];
+    } if (_.isObject(value1) && _.isObject(value2)) {
+      return [...acc, { key, value: buildTree(value1, value2), type: 'hasChild' }];
     }
-  });
-  return resultObj;
+    return [
+      ...acc,
+      {
+        key,
+        oldValue: value1,
+        value: value2,
+        type: 'changed',
+      },
+    ];
+  }, []);
 };
 
 export default buildTree;
